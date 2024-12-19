cd3e6e6aca9e16a3305ccc70607ba4ffa748af85
Mikhail Kotliarov <153776012+karmichael228@users.noreply.github.com>
2024-12-19 03:01:50 +0300

    Update parallel_shell_sort.cpp

--- parallel_shell_sort.cpp
+++ parallel_shell_sort.cpp
@@ -7,7 +7,7 @@
 void shell_sort_parallel(std::vector<int>& arr) {
     int n = arr.size();
     for (int gap = n / 2; gap > 0; gap /= 2) {
-        #pragma omp parallel for
+#pragma omp parallel for schedule(dynamic, 1)
         for (int i = gap; i < n; i++) {
             int temp = arr[i];
             int j = i;
@@ -21,16 +21,22 @@ void shell_sort_parallel(std::vector<int>& arr) {
 }
 
 int main() {
-    int n = 200000000;
+
+    int n = 30000000;
     std::vector<int> arr(n);
     std::random_device rd;
     std::mt19937 gen(rd());
     std::uniform_int_distribution<> dis(1, 1000000);
 
+#pragma omp parallel for
     for (int i = 0; i < n; i++) {
         arr[i] = dis(gen);
     }
-    omp_set_num_threads(6);  
+
+    omp_set_num_threads(8); 
+    omp_set_nested(1);  
+    omp_set_dynamic(0);
+
     auto start = std::chrono::high_resolution_clock::now();
     shell_sort_parallel(arr);
     auto end = std::chrono::high_resolution_clock::now();
